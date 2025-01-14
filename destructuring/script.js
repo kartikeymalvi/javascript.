d6e7cccd76c8218614582a60cbe876c838b469d5
kartikeymalvi <kartikeymalviya02@gmail.com>
2025-01-14 12:36:57 +0530

    destructuring in JS

---
+++ destructuring/script.js
@@ -0,0 +1,11 @@
+const student ={
+    "rollno":123,
+    "name":"kartikey",
+     "age":25
+}
+
+const{rollno,name,age}=student
+
+document.getElementById("demo").innerHTML=rollno;
+document.getElementById("demo1").innerHTML=name;
+document.getElementById("demo2").innerHTML=age;
